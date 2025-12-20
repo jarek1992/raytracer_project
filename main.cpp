b88707b7895bdf80e5d5487ae9f9e9d6de7d1ba9
Jarek Jeda <jarek.jeda@gmail.com>
2025-12-20 13:41:42 +0100

    instances

--- main.cpp
+++ main.cpp
@@ -17,33 +17,31 @@ int main() {
 	//set up a sphere into world
 	hittable_list world;
 
-	//base material and plane
+	//0. base material and plane
 	auto material_ground = make_shared<metal>(color(0.2, 0.2, 0.2), 0.3);
 	
 	world.add(make_shared<sphere>(point3(0.0, -1000.0, 0.0), 1000.0, material_ground));
 
-	//glass sphere into scene
+	//1. glass sphere into scene
 	auto material_glass = make_shared<dielectric>(1.5);
 	world.add(make_shared<sphere>(point3(0.0, 1.0, 0.0), 1.0, material_glass));
 
-	//bubble sphere into scene
+	//2. bubble sphere into scene
 	auto material_bubble = make_shared<dielectric>(1.0 / 1.5);
 	
 	world.add(make_shared<sphere>(point3(0.0, 1.0, 0.0), 0.9, material_bubble));
 
-	//cube 
+	//3. cube 
 	/*auto cube_material = make_shared<lambertian>(color(0.2, 0.5, 0.5));*/
-
 	auto texture = make_shared<image_texture>("assets/textures/fine-wood.jpg");
 	auto textured_lambertian = make_shared<lambertian>(texture);
-
 	auto cube1 = make_shared<cube>(point3(0.0, 0.0, 0.0), textured_lambertian);
 	auto rotated = make_shared<rotate_y>(cube1, 30.0);
 	auto final_pos = make_shared<translate>(rotated, point3(3.0, 1.0, 3.0));
 	
 	world.add(final_pos);
 
-	//triangle
+	//4. triangle
 	auto triangle_material = make_shared<lambertian>(color(0.8, 0.3, 0.3));
 	point3 v0(-1.0, 0.0, 0.0);
 	point3 v1(1.0, 0.0, 0.0);
@@ -54,99 +52,32 @@ int main() {
 
 	world.add(triangle_final_pos);
 
-	//metal material sphere into scene
-
+	//5. metal material sphere into scene
 	//emmissive material
 	auto emmissive_material = make_shared<diffuse_light>(color(15.0, 15.0, 15.0)); //bright white light
-
 	/*auto material_metal = make_shared<metal>(color(0.2, 0.2, 0.2), 0.2);*/
-
 	world.add(make_shared<sphere>(point3(6.0, 1.0, -2.0), 1.0, emmissive_material));
 
-	//randomize location of small spheres and cubes
+	//common materials for small cubes
+	auto mat_diffuse = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	auto mat_wood = make_shared<lambertian>(make_shared<image_texture>("assets/textures/fine-wood.jpg"));
+	//6. single cube in the scene
+	//centered at (0.0, 0.0, 0.0) 
+	auto master_cube = make_shared<cube>(point3(-0.2, -0.2, -0.2), point3(0.2, 0.2, 0.2), mat_diffuse);
+
+	//for loop to randomize location of small cubes
 	for (int a = -20; a < 20; a++) {
 		for (int b = -20; b < 20; b++) { //create the grid a(-20, 20) / b(-20, 20)
-			auto choose_material = random_double(); //randomize material for each sphere/cube in range(0, 1)
 			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double()); //point3(x,(y = const),z)
 
-			if ((center - point3(5.0, 1.0, 0.0)).length() > 0.9) { //condition to create sphere or cube
-				shared_ptr<material> object_material;
+			if ((center - point3(5.0, 1.0, 0.0)).length() > 0.9) { //condition to create cube
+				//instance of cube
+				//using rotate_y and translate as a wrapper for master_cube
+				auto rotated_cube = make_shared<rotate_y>(master_cube, random_double(0.0, 90.0));
+				auto final_instance = make_shared<translate>(rotated_cube, center);
 
-				if (choose_material < 0.8) {
-					//diffuse material
-					auto albedo = color::random() * color::random(); //random color
-					object_material = make_shared<lambertian>(albedo);
-
-					if (random_double() < 0.5) {
-						//crete sphere
-						world.add(make_shared<sphere>(center, 0.2, object_material));
-					}
-					else {
-						//create cube centered at center with side 0.4
-						auto c = make_shared<cube>(
-							point3(-0.2, -0.2, -0.2),
-							point3(0.2, 0.2, 0.2),
-							object_material
-						);
-						double angle_deg = random_double(0.0, 90.0);
-						double angle_rad = degrees_to_radians(angle_deg);
-						auto rotated = make_shared<rotate_y>(c, angle_rad); //random rotation
-						vec3 displacement = center;
-						auto final_obj = make_shared<translate>(rotated, displacement); //move cube to center position
-
-						world.add(final_obj);
-					}
-				}
-				else if (choose_material < 0.95) {
-					//metal material
-					auto albedo = color::random(0.5, 1); //range (0.5, 1) allows to select amongst brighter range
-					auto fuzz = random_double(0, 0.5); //higher value more diffused reflections
-					object_material = make_shared<metal>(albedo, fuzz);
-
-					if (random_double() < 0.5) {
-						//crete sphere
-						world.add(make_shared<sphere>(center, 0.2, object_material));
-					}
-					else {
-						//create cube centered at center with side 0.4
-						auto c = make_shared<cube>(
-							point3(-0.2, -0.2, -0.2),
-							point3(0.2, 0.2, 0.2),
-							object_material
-						);
-						double angle_deg = random_double(0.0, 90.0);
-						double angle_rad = degrees_to_radians(angle_deg);
-						auto rotated = make_shared<rotate_y>(c, angle_rad); //random rotation
-						vec3 displacement = center;
-						auto final_obj = make_shared<translate>(rotated, displacement); //move cube to center position
-
-						world.add(final_obj);
-					}
-				}
-				else {
-					//glass material
-					object_material = make_shared<dielectric>(1.5);
-
-					if (random_double() < 0.5) {
-						//crete sphere
-						world.add(make_shared<sphere>(center, 0.2, object_material));
-					}
-					else {
-						//create cube centered at center with side 0.4
-						auto c = make_shared<cube>(
-							point3(-0.2, -0.2, -0.2),
-							point3(0.2, 0.2, 0.2),
-							object_material
-						);
-						double angle_deg = random_double(0.0, 90.0);
-						double angle_rad = degrees_to_radians(angle_deg);
-						auto rotated = make_shared<rotate_y>(c, angle_rad); //random rotation
-						vec3 displacement = center;
-						auto final_obj = make_shared<translate>(rotated, displacement); //move cube to center position
-
-						world.add(final_obj);
-					}
-				}
+				//add to world
+				world.add(final_instance);
 			}
 		}
 	}
@@ -156,9 +87,9 @@ int main() {
 
 	//image aspects ratio
 	cam.aspect_ratio = 16.0 / 9.0;
-	cam.image_width = 600;
-	cam.samples_per_pixel = 10;
-	cam.max_depth = 10;
+	cam.image_width = 800;
+	cam.samples_per_pixel = 100;
+	cam.max_depth = 30;
 
 	//camera settings
 	cam.vfov = 30; //vertical field of the view
